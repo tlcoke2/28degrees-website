5a464a523a46f8d1da6e4cfad02907d26faf8563
tlcoke2 <tlcoke@hotmail.com>
2025-07-22 01:21:27 -0500

    fix: remove duplicate Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AppLayout from './components/Layout/AppLayout';
 import Home from './pages/Home';
 import Tours from './pages/Tours';
@@ -8,18 +8,16 @@ import Contact from './pages/Contact';
 import Admin from './pages/Admin';
 
 const App = () => (
-  <Router>
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tours" element={<Tours />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </AppLayout>
-  </Router>
+  <AppLayout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  </AppLayout>
 );
 
 export default App;
